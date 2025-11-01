c54154265b549be991fa9ed32dba807859d6623a
grujo85 <vladimirgrujovic85@gmail.com>
2025-11-01 20:37:35 +0100

    Add files via upload

---
+++ lista5.py
@@ -0,0 +1,23 @@
+voce=["Banana", "Ananas","kokos", "Jabuke"]
+
+
+#----------------KAKO ODREDJENI ELEMENT POSTOJI U LISTI
+#if "Banana" in voce:
+#	print("Element voce postoji u listi ")
+
+#----------------KADA ZELIMO DA OBRISEMO ELENTE U LISTI
+
+#a = voce
+#b = voce
+
+#a.clear()
+#print("Clanovi u a: ", a)
+
+#---------------------------SADA BRISEMO SVE TRI LISTE
+
+a = voce
+b = voce
+
+del a
+del b
+del voce
